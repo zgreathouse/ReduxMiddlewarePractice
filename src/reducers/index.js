aa184586a662805820c75935be61fff7ae1eeaf0
zgreathouse <greathouse.zachary@gmail.com>
2018-01-15 19:34:36 -0800

    create usersReducer and import reducer into the reducers/index.js file and wire it up to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import usersReducer from './usersReducer';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  users: usersReducer
 });
 
 export default rootReducer;
