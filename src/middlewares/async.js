98da34e584ca5bfa2593f6fcd7386e987592bf7b
zgreathouse <greathouse.zachary@gmail.com>
2018-01-16 19:48:46 -0800

    refactor middleware (handleAsync.js) to handle actions with promises

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,8 +1,18 @@
 const handleAsync = ({ dispatch }) => {
   return next => action => {
-    console.log(action);
+    //If the action does not have a payload, or the payload
+    //does not have a .then property we don't care about it,
+    //send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
 
-    next(action);
+    //Make sure the action's promise resolves
+    action.payload
+      .then(response => {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+      });
   };
 }
 
